6c7d519c84250d6cbe459bbb30bc82534c4a6c3c
Rioger <Rioger@192.168.1.129>
2016-12-21 02:46:05 +0100

    Creando la clase NaturalNumberEN

---
+++ src/main/java/es/upm/miw/pd/factoryMethod/naturalNumber/solution/NaturalNumberEn.java
@@ -0,0 +1,12 @@
+package es.upm.miw.pd.factoryMethod.naturalNumber.solution;
+
+public class NaturalNumberEn extends NaturalNumber {
+	
+    public NaturalNumberEn(int value) {
+		super(value);
+		String[] textValue = {"zero", "one", "two", "three", "four", "five"};
+		this.setTextValue(textValue);
+		
+	}
+
+}
