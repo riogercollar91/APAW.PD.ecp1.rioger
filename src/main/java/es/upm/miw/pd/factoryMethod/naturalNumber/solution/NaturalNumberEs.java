e263e8f34c6916c4c16d5dfa019c834569939ad0
Rioger <Rioger@192.168.1.129>
2016-12-21 02:41:09 +0100

    Picando la clase NaturalNumberEs

---
+++ src/main/java/es/upm/miw/pd/factoryMethod/naturalNumber/solution/NaturalNumberEs.java
@@ -0,0 +1,12 @@
+package es.upm.miw.pd.factoryMethod.naturalNumber.solution;
+
+public class NaturalNumberEs extends NaturalNumber {
+	
+    public NaturalNumberEs(int value) {
+		super(value);
+		String[] textValue = {"cero", "uno", "dos", "tres", "cuatro", "cinco"};
+		this.setTextValue(textValue);
+		
+	}
+
+}
